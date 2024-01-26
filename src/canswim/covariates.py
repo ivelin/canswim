025341881d0bbc1f79bf15c4148709434ce5eba5
ivelin <ivelin117@gmail.com>
2024-01-26 14:03:12 -0600

    bug fixes
    Signed-off-by: ivelin <ivelin117@gmail.com>

--- src/canswim/covariates.py
+++ src/canswim/covariates.py
@@ -64,7 +64,8 @@ class Covariates:
 
     def prepare_earn_series(self, tickers=None):
         # convert date strings to numerical representation
-        earn_df = self.earnings_loaded_df
+        earn_df = self.earnings_loaded_df.copy()
+        print("self.earnings_loaded_df.columns", self.earnings_loaded_df.columns)
         ufd = pd.to_datetime(earn_df["updatedFromDate"])
         ufd_year = ufd.dt.year
         ufd_month = ufd.dt.month
@@ -122,7 +123,7 @@ class Covariates:
 
         return t_earn_series
 
-    def stack_covariates(old_covs=None, new_covs=None, min_samples=1):
+    def stack_covariates(self, old_covs=None, new_covs=None, min_samples=1):
         # stack sales and earnigns to past covariates
         stacked_covs = {}
         for t, covs in list(old_covs.items()):
@@ -143,12 +144,12 @@ class Covariates:
                 print(f"Skipping {t} covariates stack due to error: ", e)
         return stacked_covs
 
-    def df_index_to_biz_days(df=None):
+    def df_index_to_biz_days(self, df=None):
         new_index = df.index.map(lambda x: to_biz_day(date=x))
         df.index = new_index
         return df
 
-    def pad_kms(kms_series=None, price_series=None):
+    def pad_kms(self, kms_series=None, price_series=None):
         """
         Pad a ticker's key metrics to align with price data
         """
@@ -173,7 +174,7 @@ class Covariates:
         return updated_kms_series
 
     def prepare_key_metrics(self, stock_price_series=None):
-        kms_loaded_df = self.kms_loaded_df
+        kms_loaded_df = self.kms_loaded_df.copy()
         # print(kms_loaded_df)
         assert kms_loaded_df.index.is_unique
         kms_loaded_df.index
@@ -222,9 +223,8 @@ class Covariates:
         # print("t_kms_series:", t_kms_series)
         return t_kms_series
 
-    def prepare_broad_market_indicators(csv_file: str = None, train_date_start=None):
-        broad_market_df = pd.read_csv(csv_file, header=[0, 1], index_col=0)
-
+    def prepare_broad_market_series(self, csv_file: str = None, train_date_start=None):
+        broad_market_df = self.broad_market_df.copy()
         # flatten column hierarchy so Darts can use as covariate series
         broad_market_df.columns = [f"{i}_{j}" for i, j in broad_market_df.columns]
 
@@ -263,53 +263,70 @@ class Covariates:
         self.data_loaded = True
 
     def load_past_covariates(self):
-        self.earnings_loaded_df = self.load_earnings()
-        self.kms_loaded_df = self.load_key_metrics()
+        self.load_earnings()
+        self.load_key_metrics()
+        self.load_broad_market()
+
+    def load_broad_market(self):
+        csv_file = "data/broad_market.csv.bz2"
+        self.broad_market_df = pd.read_csv(csv_file, header=[0, 1], index_col=0)
 
     def load_future_covariates(self):
-        self.est_quarter_loaded_df = self.load_estimates(period="quarter")
-        self.est_annual_loaded_df = self.load_estimates(period="annual")
+        self.load_estimates()
 
-    def prepare_data(self):
+    def prepare_data(
+        self,
+        stock_price_series: dict = None,
+        target_columns: Union[str, list] = None,
+        train_date_start: pd.Timestamp = None,
+        min_samples: int = None,
+    ):
         assert (
             self.data_loaded is True
         ), "Data needs to be loaded before it can be prepared. Make sure to first call load_data()"
-        self.prepare_past_covariates()
-        self.prepare_future_covariates()
+        self.prepare_past_covariates(
+            stock_price_series=stock_price_series,
+            target_columns=target_columns,
+            train_date_start=train_date_start,
+        )
+        self.prepare_future_covariates(
+            stock_price_series=stock_price_series, min_samples=min_samples
+        )
 
     def load_earnings(self):
         earnings_csv_file = "data/earnings_calendar.csv.bz2"
         earnings_loaded_df = pd.read_csv(earnings_csv_file)
-        earnings_loaded_df["date"] = pd.to_datetime(self.earnings_loaded_df["date"])
-        earnings_unique = self.earnings_loaded_df.drop_duplicates(
-            subset=["symbol", "date"]
-        )
+        print("earnings_loaded_df.columns", earnings_loaded_df.columns)
+        earnings_loaded_df["date"] = pd.to_datetime(earnings_loaded_df["date"])
+        earnings_unique = earnings_loaded_df.drop_duplicates(subset=["symbol", "date"])
         assert not earnings_unique.duplicated().any()
         earnings_unique = earnings_unique.set_index(keys=["symbol", "date"])
         assert earnings_unique.index.has_duplicates == False
-        return earnings_unique
+        self.earnings_loaded_df = earnings_unique
 
     def load_key_metrics(self):
         kms_file = "data/keymetrics_history.csv.bz2"
         kms_loaded_df = pd.read_csv(kms_file)
-        return kms_loaded_df
+        self.kms_loaded_df = kms_loaded_df
 
-    def load_estimates(self, period: str = None):
-        assert period in fiscal_periods
-        est_file = f"data/analyst_estimates_{period}.csv.bz2"
-        est_loaded_df = pd.read_csv(est_file)
-        assert est_loaded_df.index.is_unique
-        # print(f'{period} estimates loaded: \n{est_loaded_df}')
-        est_loaded_df["date"] = pd.to_datetime(est_loaded_df["date"])
-        est_unique = est_loaded_df.drop_duplicates(subset=["symbol", "date"])
-        assert not est_unique.duplicated().any()
-        est_unique = est_unique.set_index(keys=["symbol", "date"])
-        assert est_unique.index.has_duplicates == False
-        assert est_unique.index.is_unique == True
-        # print(f'{period} estimates prepared: \n{est_unique}')
-        return est_unique
+    def load_estimates(self):
+        self.est_loaded_df = {}
+        for period in fiscal_periods:
+            assert period in fiscal_periods
+            est_file = f"data/analyst_estimates_{period}.csv.bz2"
+            est_loaded_df = pd.read_csv(est_file)
+            assert est_loaded_df.index.is_unique
+            # print(f'{period} estimates loaded: \n{est_loaded_df}')
+            est_loaded_df["date"] = pd.to_datetime(est_loaded_df["date"])
+            est_unique = est_loaded_df.drop_duplicates(subset=["symbol", "date"])
+            assert not est_unique.duplicated().any()
+            est_unique = est_unique.set_index(keys=["symbol", "date"])
+            assert est_unique.index.has_duplicates == False
+            assert est_unique.index.is_unique == True
+            # print(f'{period} estimates prepared: \n{est_unique}')
+            self.est_loaded_df[period] = est_unique
 
-    def est_add_future_periods(est_df=None, n_future_periods=None, period=None):
+    def est_add_future_periods(self, est_df=None, n_future_periods=None, period=None):
         """
         Prepare time series with concatenated estimates for n_future_periods
         """
@@ -392,14 +409,13 @@ class Covariates:
         assert self.est_annual_loaded_df is not None
         q_loaded_df = self.est_quarter_loaded_df
         quarter_est_series = self.prepare_est_series(
-            all_est_df=q_loaded_df,
+            all_est_df=self.est_loaded_df["quarter"],
             n_future_periods=4,
             period="quarter",
             tickers=tickers,
         )
-        annual_loaded_df = self.est_annual_loaded_df
         annual_est_series = self.prepare_est_series(
-            all_est_df=annual_loaded_df,
+            all_est_df=self.est_loaded_df["annual"],
             n_future_periods=2,
             period="annual",
             tickers=tickers,
@@ -407,7 +423,10 @@ class Covariates:
         return quarter_est_series, annual_est_series
 
     def prepare_past_covariates(
-        self, stock_price_series: dict = None, target_columns: Union[str, list] = None
+        self,
+        stock_price_series: dict = None,
+        target_columns: Union[str, list] = None,
+        train_date_start: pd.Timestamp = None,
     ):
         # start with price-volume covariates
         price_covariates = self.get_price_covariates(
@@ -423,47 +442,41 @@ class Covariates:
         # add key metrics covariates
         kms_series = self.prepare_key_metrics(stock_price_series=stock_price_series)
         past_covariates = self.stack_covariates(
-            old_covs=past_covariates, new_covs=kms_series
-        )
-        print(
-            f"past covariates with key metrics sample: \n{past_covariates[t][0:5].pd_dataframe()}"
+            old_covs=self.past_covariates, new_covs=kms_series
         )
         # Add broad market indicators to past covariates
-        broad_market_series = self.prepare_broad_market_indicators(
-            csv_file="data/broad_market.csv.bz2", train_date_start=train_date_start
+        broad_market_series = self.prepare_broad_market_series(
+            train_date_start=train_date_start
         )
-        broad_market_dict = {t: broad_market_series for t in target_series.keys()}
+        broad_market_dict = {t: broad_market_series for t in stock_price_series.keys()}
         past_covariates_tmp = self.stack_covariates(
             old_covs=past_covariates, new_covs=broad_market_dict
         )
         past_covariates = past_covariates_tmp
-        return past_covariates
+        self.past_covariates = past_covariates
 
-    def __add_holidays(self, t_series: dict = None):
+    def __add_holidays(self, series_dict: dict = None):
         new_series = {}
-        for t, series in t_series.items():
+        for t, series in series_dict.items():
             series_with_holidays = series.add_holidays(country_code="US")
             new_series[t] = series_with_holidays
             # print(f'ticker: {t} , {ticker_series[t]}')
         print("Added holidays to ticker series.")
         return new_series
 
-    def prepare_future_covariates(self, ticker_series=None, min_samples=None):
+    def prepare_future_covariates(
+        self, stock_price_series: {} = None, min_samples=None
+    ):
         # add analyst estimates
         quarter_est_series, annual_est_series = self.prepare_analyst_estimates(
-            csv_file_quarter="data/analyst_estimates_quarter.csv.bz2",
-            csv_file_annual="data/analyst_estimates_annual.csv.bz2",
-            tickers=ticker_series.keys(),
+            tickers=stock_price_series.keys(),
         )
         ## Stack analyst estimates to future covariates
         stacked_future_covariates = self.stack_covariates(
-            old_covs=future_covariates, new_covs=quarter_est_series
-        )
-        stacked_future_covariates = self.stack_covariates(
-            old_covs=stacked_future_covariates,
+            old_covs=quarter_est_series,
             new_covs=annual_est_series,
             min_samples=min_samples,
         )
         future_covariates = stacked_future_covariates
         future_covariates = self.__add_holidays(future_covariates)
-        return future_covariates
+        self.future_covariates = future_covariates
