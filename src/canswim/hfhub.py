2e928702f8465427b4370c14bf4a80d1fd7f7e79
ivelin <ivelin117@gmail.com>
2024-02-01 08:57:55 -0600

    cleanup hfhub utils

--- src/canswim/hfhub.py
+++ src/canswim/hfhub.py
@@ -3,34 +3,40 @@ from dotenv import load_dotenv
 import os
 import tempfile
 from typing import Optional
+from darts import TimeSeries
 from darts.models.forecasting.forecasting_model import ForecastingModel
-from huggingface_hub import snapshot_download, upload_folder
+from huggingface_hub import snapshot_download, upload_folder, create_repo
 
 
 class HFHub:
-    """HuggingFace Hub integration"""
+    """
+    HuggingFace Hub integration using official HF API.
+    https://huggingface.co/docs/huggingface_hub/v0.20.3/en/guides/integrations
+    """
 
     def __init__(self, api_key: Optional[str] = None):
-        print("HFHub init")
         if api_key is None:
             # load from .env file or OS vars if available
             load_dotenv(override=True)
-            api_key = os.getenv("HF_HUB_TOKEN")
+            api_key = os.getenv("HF_TOKEN")
             assert (
                 api_key is not None
-            ), "Could not find HF_HUB_TOKEN in OS environment. Cannot interact with HF Hub."
-        self.HF_HUB_TOKEN = api_key
+            ), "Could not find HF_TOKEN in OS environment. Cannot interact with HF Hub."
+        self.HF_TOKEN = api_key
 
-    def push_data(df: pd.DataFrame, url: str = None):
-        pass
+    def upload_model(
+        self,
+        model: ForecastingModel = None,
+        repo_id: str = None,
+        private: Optional[bool] = True,
+    ):
+        # Create repo if not existing yet and get the associated repo_id
+        create_repo(repo_id=repo_id, repo_type="model", private=private, exist_ok=True)
 
-    def upload_model(self, model: ForecastingModel = None, repo_id: str = None):
         with tempfile.TemporaryDirectory() as tmpdirname:
-            print("created temporary directory for model", tmpdirname)
+            # print("created temporary directory for model", tmpdirname)
             model.save(path=f"{tmpdirname}/{model.model_name}")
-            upload_folder(
-                repo_id=repo_id, folder_path=tmpdirname, token=self.HF_HUB_TOKEN
-            )
+            upload_folder(repo_id=repo_id, folder_path=tmpdirname, token=self.HF_TOKEN)
 
     def download_model(
         self,
@@ -40,7 +46,28 @@ class HFHub:
     ) -> ForecastingModel:
         with tempfile.TemporaryDirectory() as tmpdirname:
             snapshot_download(
-                repo_id=repo_id, local_dir=tmpdirname, token=self.HF_HUB_TOKEN
+                repo_id=repo_id, local_dir=tmpdirname, token=self.HF_TOKEN
             )
             model = model_class.load(path=f"{tmpdirname}/{model_name}")
             return model
+
+    def upload_timeseries(
+        self,
+        series: TimeSeries = None,
+        repo_id: str = None,
+        private: Optional[bool] = True,
+    ):
+        # Create repo if not existing yet and get the associated repo_id
+        create_repo(
+            repo_id=repo_id, repo_type="datasets", private=private, exist_ok=True
+        )
+
+        pass
+
+    def download_timeseries(
+        self,
+        series_name: str = None,
+        repo_id: str = None,
+        private: Optional[bool] = True,
+    ) -> TimeSeries:
+        pass
