ac55556d8e3461111b8772adf602ee207939df2a
ivelin <ivelin117@gmail.com>
2024-02-08 18:17:14 +0000

    fix multi user web app access and future covariates end date
    Signed-off-by: ivelin <ivelin117@gmail.com>

--- src/canswim/dashboard.py
+++ src/canswim/dashboard.py
@@ -17,6 +17,23 @@ repo_id = "ivelin/canswim"
 
 class CanswimPlayground():
 
+
+    def get_target(self, ticker):
+        target = self.canswim_model.targets.target_series[ticker]
+        print('target {ticker} start, end: ', target.start_time(), target.end_time())
+        return target
+
+    def get_past_covariates(self, ticker):
+        past_covariates = self.canswim_model.covariates.past_covariates[ticker]
+        print('past covariates start, end: ', past_covariates.start_time(), past_covariates.end_time())
+        return past_covariates
+
+    def get_future_covariates(self, ticker):
+        future_covariates = self.canswim_model.covariates.future_covariates[ticker]
+        print('future covariates start, end: ', future_covariates.start_time(), future_covariates.end_time())
+        return future_covariates
+
+
     def __init__(self):
         self.canswim_model = CanswimModel()
         self.hfhub = HFHub()
@@ -42,62 +59,63 @@ class CanswimPlayground():
         self.tickers = list(self.canswim_model.targets.target_series.keys())
 
 
-    def plot_forecast(self, lowq: int = 0.2):
+    def plot_forecast(self, ticker: str = None, lowq: int = 0.2):
+        target = self.get_target(ticker)
+        baseline_forecast, canswim_forecast = self.get_forecast(ticker)
+        backtest_forecasts = self.backtest(ticker)
         lq = lowq/100
         fig, axes = pyplot.subplots(figsize=(20, 12))
-        self.target.plot(label=f"{self.ticker} Close actual")
-        self.baseline_forecast.plot(label=f"{self.ticker} Close baseline forecast")
-        self.canswim_forecast.plot(label=f"{self.ticker} Close CANSWIM forecast", low_quantile=lq, high_quantile=0.95)
-        for b in self.backtest_forecasts:
-            b.plot(label=f"{self.ticker} Close CANSWIM backtest", low_quantile=lq, high_quantile=0.95)
+        target.plot(label=f"{ticker} Close actual")
+        baseline_forecast.plot(label=f"{ticker} Close baseline forecast")
+        canswim_forecast.plot(label=f"{ticker} Close CANSWIM forecast", low_quantile=lq, high_quantile=0.95)
+        for b in backtest_forecasts:
+            b.plot(label=f"{ticker} Close CANSWIM backtest", low_quantile=lq, high_quantile=0.95)
         pyplot.legend()
         return fig
 
 
-    def get_forecast(self, ticker: str = None, lowq: int = 20):
-        self.ticker = ticker
-        self.target = self.canswim_model.targets.target_series[ticker]
-        self.past_covariates = self.canswim_model.covariates.past_covariates[ticker]
-        self.future_covariates = self.canswim_model.covariates.future_covariates[ticker]
-        print('target {ticker} start, end: ', self.target.start_time(), self.target.end_time())
-        print('future covariates start, end: ', self.future_covariates.start_time(), self.future_covariates.end_time())
+    def get_forecast(self, ticker: str = None):
+        target = self.get_target(ticker)
+        past_covariates = self.get_past_covariates(ticker)
+        future_covariates = self.get_future_covariates(ticker)
         baseline_model = ExponentialSmoothing()
-        baseline_model.fit(self.target)
-        self.baseline_forecast = baseline_model.predict(self.canswim_model.pred_horizon, num_samples=500)
+        baseline_model.fit(target)
+        baseline_forecast = baseline_model.predict(self.canswim_model.pred_horizon, num_samples=500)
         cached_canswim_pred = self.forecast_cache.get(ticker)
         if cached_canswim_pred is not None:
             print(f"{ticker} forecast found in cache.")
-            self.canswim_forecast = cached_canswim_pred
+            canswim_forecast = cached_canswim_pred
         else:
             print(f"{ticker} forecast not in cache. Running model predict().")
-            self.canswim_forecast = self.canswim_model.predict(target=[self.target], past_covariates=[self.past_covariates], future_covariates=[self.future_covariates])[0]
-            self.forecast_cache[ticker] = self.canswim_forecast
-        self.backtest()
-        fig = self.plot_forecast(lowq=lowq)
+            canswim_forecast = self.canswim_model.predict(target=[target], past_covariates=[past_covariates], future_covariates=[future_covariates])[0]
+            self.forecast_cache[ticker] = canswim_forecast
         print(f"{ticker} get_forecast() finished.")
-        return fig
+        return baseline_forecast, canswim_forecast
 
-    def backtest(self):
-        cached_backtest = self.backtest_cache.get(self.ticker)
+    def backtest(self, ticker: str = None):
+        cached_backtest = self.backtest_cache.get(ticker)
         if cached_backtest is not None:
-            print(f"{self.ticker} backtest found in cache.")
-            self.backtest_forecasts = cached_backtest
+            print(f"{ticker} backtest found in cache.")
+            backtest_forecasts = cached_backtest
         else:
-            print(f"{self.ticker} backtest not in cache. Running model predict().")
-            end_date = self.target.end_time()
+            print(f"{ticker} backtest not in cache. Running model predict().")
+            target = self.get_target(ticker)
+            past_covariates = self.get_past_covariates(ticker)
+            future_covariates = self.get_future_covariates(ticker)
+            end_date = target.end_time()
             earnings_df = self.canswim_model.covariates.earnings_loaded_df
             print("earnings_df.columns", earnings_df.columns)
-            mask = (earnings_df.index.get_level_values('Symbol') == self.ticker) & (earnings_df.index.get_level_values('Date') < end_date - BDay(n=10))
+            mask = (earnings_df.index.get_level_values('Symbol') == ticker) & (earnings_df.index.get_level_values('Date') < end_date - BDay(n=10))
             earnings_dates = earnings_df.loc[mask]
-            print(f"{self.ticker} earnings dates: {earnings_dates}")
+            print(f"{ticker} earnings dates: {earnings_dates}")
             earnings_dates_unique = earnings_dates.index.get_level_values('Date').unique()
             assert len(earnings_dates_unique) >= 2
-            target1 = self.target.drop_after(earnings_dates_unique[-1])
-            target2 = self.target.drop_after(earnings_dates_unique[-2])
-            bt = self.canswim_model.predict(target=[target1, target2], past_covariates=[self.past_covariates, self.past_covariates], future_covariates=[self.future_covariates, self.future_covariates])
-            print(f"{self.ticker} backtest finished.\n", bt)
-            self.backtest_forecasts = bt
-            self.backtest_cache[self.ticker] = bt
+            target1 = target.drop_after(earnings_dates_unique[-1])
+            target2 = target.drop_after(earnings_dates_unique[-2])
+            backtest_forecasts = self.canswim_model.predict(target=[target1, target2], past_covariates=[past_covariates, past_covariates], future_covariates=[future_covariates, future_covariates])
+            print(f"{ticker} backtest finished.\n", backtest_forecasts)
+            self.backtest_cache[ticker] = backtest_forecasts
+        return backtest_forecasts
 
 
 with gr.Blocks() as demo:
@@ -121,9 +139,9 @@ with gr.Blocks() as demo:
         ## time = gr.Dropdown(["3 months", "6 months", "9 months", "12 months"], label="Downloads over the last...", value="12 months")
         lowq = gr.Slider(5, 80, value=20, label="Forecast probability low threshold", info="Choose from 5% to 80%")
 
-    tickerDropdown.change(fn=canswim_playground.get_forecast, inputs=[tickerDropdown, lowq], outputs=[plotComponent], queue=False)
-    lowq.change(fn=canswim_playground.plot_forecast, inputs=[lowq], outputs=[plotComponent], queue=False)
+    tickerDropdown.change(fn=canswim_playground.plot_forecast, inputs=[tickerDropdown, lowq], outputs=[plotComponent], queue=False)
+    lowq.change(fn=canswim_playground.plot_forecast, inputs=[tickerDropdown, lowq], outputs=[plotComponent], queue=False)
     ## time.change(get_forecast, [lib, time], plt, queue=False)
-    demo.load(fn=canswim_playground.get_forecast, inputs=[tickerDropdown, lowq], outputs=[plotComponent], queue=False)
+    demo.load(fn=canswim_playground.plot_forecast, inputs=[tickerDropdown, lowq], outputs=[plotComponent], queue=False)
 
 demo.launch()
