acbc383571a2425047108866d743c0fc351fb043
ivelin <ivelin117@gmail.com>
2024-03-09 13:03:51 -0600

    fix: bugs in data ingestion and cleanup
    Signed-off-by: ivelin <ivelin117@gmail.com>

--- src/canswim/gather_data.py
+++ src/canswim/gather_data.py
@@ -137,8 +137,6 @@ class MarketDataGatherer:
                 "NQH4",
                 "XTSLA",
                 "-",
-                "PDLI",
-                "ADRO",
                 "ICSUAGD",
                 "BFB",
                 "GTXI",
@@ -146,24 +144,20 @@ class MarketDataGatherer:
                 "LGFB",
                 "MLIFT",
                 "ESH4",
-                "LGFA",
-                "MOGA",
-                "PBRA",
-                "BRKB",
                 "RTYH4",
                 "\xa0",
-                "CRDA",
                 "GBP",
-                "ESMT",
-                "MRTX",
-                "RAIN",
                 "CAD",
                 "EUR",
+                "---",
+                "1766401D",
+                "BF/A",
+                "BF/B",
             ]
         )
-        self.stocks_ticker_set = self.stocks_ticker_set - junk_tickers
+        self.stocks_ticker_set = sorted(list(self.stocks_ticker_set - junk_tickers))
         stocks_df = pd.DataFrame()
-        stocks_df["Symbol"] = list(self.stocks_ticker_set)
+        stocks_df["Symbol"] = self.stocks_ticker_set
         stocks_df = stocks_df.set_index(["Symbol"])
         stocks_df.index = stocks_df.index.drop_duplicates()
         stocks_df
@@ -221,8 +215,7 @@ class MarketDataGatherer:
         merged_df.to_parquet(data_file)
         logger.info(f"Saved data to {data_file}")
         _bm = pd.read_parquet(data_file)
-        assert sorted(_bm.columns) == sorted(merged_df.columns)
-        assert len(_bm) == len(merged_df)
+        pd.testing.assert_frame_equal(_bm, merged_df)
         logger.info(f"Sanity check passed for saved data. Loaded OK from {data_file}")
 
     def gather_broad_market_data(self):
@@ -294,8 +287,7 @@ class MarketDataGatherer:
         merged_df.to_parquet(data_file)
         logger.info(f"Saved broad market data to {data_file}")
         _bm = pd.read_parquet(data_file)
-        assert sorted(_bm.columns) == sorted(merged_df.columns)
-        assert len(_bm) == len(merged_df)
+        pd.testing.assert_frame_equal(_bm, merged_df)
         logger.info(
             f"Sanity check passed for broad market data. Loaded OK from {data_file}"
         )
@@ -308,16 +300,24 @@ class MarketDataGatherer:
                 apikey=self.FMP_API_KEY, symbol=ticker, limit=-1
             )
             if earnings is not None and len(earnings) > 0:
-                edf = pd.DataFrame(earnings)
-                edf = edf.dropna(how="all")
-                edf["date"] = pd.to_datetime(edf["date"])
-                edf = edf.set_index(["symbol", "date"])
-                # edf = edf.pivot(columns='symbol')
-                # edf.swaplevel(i=0,j=1, axis=0)
-                # edf.drop(columns=['symbol'])
-                earnings_all_df = pd.concat([earnings_all_df, edf])
-                # n_earnings = len(earnings)
-                # logger.info(f"Total earnings reports for {ticker}: {n_earnings}")
+                try:
+                    edf = pd.DataFrame(earnings)
+                    edf = edf.dropna(how="all")
+                    edf = edf.fillna(-1)
+                    if not edf.empty:
+                        edf["date"] = pd.to_datetime(edf["date"])
+                        edf = edf.set_index(["symbol", "date"])
+                        earnings_all_df = pd.concat([earnings_all_df, edf])
+                        logger.info(f"Total earnings reports for {ticker}: {len(edf)}")
+                    else:
+                        logger.warning(f"Skipping {ticker} due to lack of data")
+                except ValueError as e:
+                    logger.warning(
+                        f"Skipping {ticker} due to error: {type(e)}, {e}, \n{earnings} "
+                    )
+            else:
+                logger.warning(f"Skipping {ticker} due to lack of data")
+
         #    earliest_earn = earnings[-1] if len(earnings > 0 else 'None')
         #    logger.info(f"Earliest earnings report for {ticker}: {earliest_earn}")
 
@@ -336,9 +336,7 @@ class MarketDataGatherer:
         ### Read back data and verify it
         tmp_earn_df = pd.read_parquet(earnings_file)
         logger.info(f"tmp_earn_df: \n{tmp_earn_df}")
-        assert tmp_earn_df.index.names == ["Symbol", "Date"]
-        assert len(tmp_earn_df) == len(earnings_all_df)
-        assert sorted(tmp_earn_df.columns) == sorted(earnings_all_df.columns)
+        pd.testing.assert_frame_equal(tmp_earn_df, earnings_all_df)
         logger.info(
             f"Sanity check passed for earnings data. Loaded OK from file: {earnings_file}"
         )
@@ -351,36 +349,46 @@ class MarketDataGatherer:
                 apikey=self.FMP_API_KEY, symbol=ticker, period="quarter", limit=-1
             )
             if kms is not None and len(kms) > 0:
-                kms_df = pd.DataFrame(kms)
-                kms_df["date"] = pd.to_datetime(kms_df["date"])
-                kms_df = kms_df.set_index(["symbol", "date"])
-                # logger.info(f"Key metrics for {ticker} sample: \n{kms_df.columns}")
-                keymetrics_all_df = pd.concat([keymetrics_all_df, kms_df])
-                # logger.info(f"Key metrics concatenated {ticker}: \n{keymetrics_all_df.columns}")
-                n_kms = len(kms_df)
-                logger.info(f"Total key metrics reports for {ticker}: {n_kms}")
+                try:
+                    kms_df = pd.DataFrame(kms)
+                    kms_df = kms_df.dropna(how="all")
+                    kms_df = kms_df.fillna(-1)
+                    if not kms_df.empty:
+                        kms_df["date"] = pd.to_datetime(kms_df["date"])
+                        kms_df = kms_df.set_index(["symbol", "date"])
+                        # logger.info(f"Key metrics for {ticker} sample: \n{kms_df.columns}")
+                        keymetrics_all_df = pd.concat([keymetrics_all_df, kms_df])
+                        # logger.info(f"Key metrics concatenated {ticker}: \n{keymetrics_all_df.columns}")
+                        n_kms = len(kms_df)
+                        logger.info(f"Total key metrics reports for {ticker}: {n_kms}")
+                    else:
+                        logger.warning(f"Skipping {ticker} due to lack of data")
+                except ValueError as e:
+                    logger.warning(
+                        f"Skipping {ticker} due to error: {type(e)}, {e}, \n{kms} "
+                    )
             else:
                 logger.info(f"No {ticker} key metrics reports: kms={kms}")
 
-        logger.info(f"keymetrics_all_df: \n{keymetrics_all_df}")
-        logger.info(f"keymetrics_all_df.dtypes: \n{keymetrics_all_df.dtypes}")
-        logger.info(f"len(keymetrics_all_df): \n{len(keymetrics_all_df)}")
-        # index, row = next(keymetrics_all_df.iterrows())
-        # row["averagePayables"]
-        # prevent parquet serialization issues
-        keymetrics_all_df["averagePayables"] = pd.to_numeric(
-            keymetrics_all_df["averagePayables"], dtype_backend="pyarrow"
+        logger.debug(f"keymetrics_all_df: \n{keymetrics_all_df}")
+        logger.debug(f"keymetrics_all_df.dtypes: \n{keymetrics_all_df.dtypes}")
+        logger.debug(f"len(keymetrics_all_df): \n{len(keymetrics_all_df)}")
+        # cleanup data types to prevent parquet serialization issues
+        kms_num_cols = list(set(keymetrics_all_df.columns) - {"calendarYear", "period"})
+        keymetrics_all_df[kms_num_cols] = keymetrics_all_df[kms_num_cols].apply(
+            pd.to_numeric, dtype_backend="pyarrow"
         )
         keymetrics_all_df.index.names = ["Symbol", "Date"]
         keymetrics_all_df = keymetrics_all_df.sort_index()
+        logger.debug(
+            f"keymetrics_all_df.dtypes after cleanup: \n{keymetrics_all_df.dtypes}"
+        )
         kms_file = "data/data-3rd-party/keymetrics_history.parquet"
         keymetrics_all_df.to_parquet(kms_file, engine="pyarrow")
         ### Read back data and verify it
         temp_kms_df = pd.read_parquet(kms_file)
         logger.info(f"temp_kms_df: \n{temp_kms_df}")
-        assert temp_kms_df.index.names == ["Symbol", "Date"]
-        assert len(temp_kms_df) == len(keymetrics_all_df)
-        assert sorted(temp_kms_df.columns) == sorted(keymetrics_all_df.columns)
+        pd.testing.assert_frame_equal(temp_kms_df, keymetrics_all_df)
         logger.info(
             f"Sanity check passed for key metrics data. Loaded OK from file: {kms_file}"
         )
@@ -419,9 +427,7 @@ class MarketDataGatherer:
             ### Read back data and verify it
             tmp_df = pd.read_parquet(file)
             logger.debug(f"tmp_df: \n{tmp_df}")
-            assert tmp_df.index.names == ["Symbol", "Date"]
-            assert len(tmp_df) == len(all_df)
-            assert sorted(tmp_df.columns) == sorted(all_df.columns)
+            pd.testing.assert_frame_equal(tmp_df, all_df)
             logger.info(f"Sanity check passed. Data loaded OK from file: {file}")
         logger.info("Finished gathering stock dividends data.")
 
@@ -457,9 +463,7 @@ class MarketDataGatherer:
             ### Read back data and verify it
             tmp_df = pd.read_parquet(file)
             logger.debug(f"tmp_df: \n{tmp_df}")
-            assert tmp_df.index.names == ["Symbol", "Date"]
-            assert len(tmp_df) == len(all_df)
-            assert sorted(tmp_df.columns) == sorted(all_df.columns)
+            pd.testing.assert_frame_equal(tmp_df, all_df)
             logger.info(f"Sanity check passed. Data loaded OK from file: {file}")
         logger.info("Finished gathering stock splits data.")
 
@@ -545,9 +549,7 @@ class MarketDataGatherer:
         ### Read back data and verify it
         tmp_int_own_df = pd.read_parquet(inst_ownership_file)
         logger.info(f"tmp_int_own_df: \n{tmp_int_own_df}")
-        assert tmp_int_own_df.index.names == ["Symbol", "Date"]
-        assert len(tmp_int_own_df) == len(inst_own_df)
-        assert sorted(tmp_int_own_df.columns) == sorted(inst_own_df.columns)
+        pd.testing.assert_frame_equal(tmp_int_own_df, inst_own_df)
         logger.info(
             f"Sanity check passed for institutional ownership data. Loaded OK from file: {inst_ownership_file}"
         )
@@ -561,18 +563,28 @@ class MarketDataGatherer:
                 est = analyst_estimates(
                     apikey=self.FMP_API_KEY, symbol=ticker, period=period, limit=-1
                 )
-                # logger.info('est:', est)
                 if est is not None and len(est) > 0:
-                    est_df = pd.DataFrame(est)
-                    est_df["date"] = pd.to_datetime(est_df["date"])
-                    est_df = est_df.set_index(["symbol", "date"])
-                    # logger.info(f"Analyst estimates for {ticker} sample: \n{est_df.columns}")
-                    estimates_all_df = pd.concat([estimates_all_df, est_df])
-                    # logger.info(f"Key metrics concatenated {ticker}: \n{estimates_all_df.columns}")
-                    n_est = len(est_df)
-                    logger.info(
-                        f"{n_est} total {ticker} {period} analyst estimates reports"
-                    )
+                    try:
+                        # logger.debug(f"{ticker} estimates: {est}")
+                        est_df = pd.DataFrame(est)
+                        est_df = est_df.dropna(how="all")
+                        est_df = est_df.fillna(-1)
+                        if not est_df.empty:
+                            est_df["date"] = pd.to_datetime(est_df["date"])
+                            est_df = est_df.set_index(["symbol", "date"])
+                            # logger.info(f"Analyst estimates for {ticker} sample: \n{est_df.columns}")
+                            estimates_all_df = pd.concat([estimates_all_df, est_df])
+                            # logger.info(f"Key metrics concatenated {ticker}: \n{estimates_all_df.columns}")
+                            n_est = len(est_df)
+                            logger.info(
+                                f"{n_est} total {ticker} {period} analyst estimates reports"
+                            )
+                        else:
+                            logger.warning(f"Skipping {ticker} due to lack of data")
+                    except ValueError as e:
+                        logger.warning(
+                            f"Skipping {ticker} due to error: {type(e)}, {e}, \n{est} "
+                        )
                 else:
                     logger.info(
                         f"No {ticker} {period} analyst estimates reports: est={est}"
@@ -594,11 +606,7 @@ class MarketDataGatherer:
             logger.info(f"all {p} estimates count: {len(estimates_all_df)}")
             # logger.info(f"{p} estimates sample:\n{estimates_all_df}")
             tmp_est_df = pd.read_parquet(est_file_name)
-            assert tmp_est_df.index.names == ["Symbol", "Date"]
-            assert len(tmp_est_df) == len(estimates_all_df)
-            # logger.info(f"tmp_est_df.columns: {tmp_est_df.columns}")
-            # logger.info(f"estimates_all_df.columns: {estimates_all_df.columns}")
-            assert sorted(tmp_est_df.columns) == sorted(estimates_all_df.columns)
+            pd.testing.assert_frame_equal(tmp_est_df, estimates_all_df)
             logger.info(
                 f"Sanity check passed for analyst estimates data. Loaded OK from file: {est_file_name}"
             )
@@ -609,16 +617,16 @@ def main():
     hfhub = HFHub()
     hfhub.download_data()
     g = MarketDataGatherer()
-    g.gather_broad_market_data()
-    g.gather_sectors_data()
-    g.gather_industry_fund_data()
+    # g.gather_broad_market_data()
+    # g.gather_sectors_data()
+    # g.gather_industry_fund_data()
     g.gather_stock_tickers()
-    g.gather_stock_price_data()
-    g.gather_stock_dividends()
-    g.gather_stock_splits()
-    g.gather_earnings_data()
-    g.gather_stock_key_metrics()
-    g.gather_institutional_stock_ownership()
+    # g.gather_stock_price_data()
+    # g.gather_stock_dividends()
+    # g.gather_stock_splits()
+    # g.gather_earnings_data()
+    # g.gather_stock_key_metrics()
+    # g.gather_institutional_stock_ownership()
     g.gather_analyst_estimates()
     hfhub.upload_data()
 
