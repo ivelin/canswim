e480729bd5bdea30e3501e9aa1a7ab37a944be67
ivelin <ivelin117@gmail.com>
2024-09-22 20:29:41 -0500

    feat: allow shorter data history when using model for forecasts only and no training
    Signed-off-by: ivelin <ivelin117@gmail.com>

--- forecast.sh
+++ forecast.sh
@@ -39,4 +39,5 @@ set -exv
 #./canswim.sh forecast --forecast_start_date "2024-08-26"
 #./canswim.sh forecast --forecast_start_date "2024-09-02"
 #./canswim.sh forecast --forecast_start_date "2024-09-09"
-./canswim.sh forecast --forecast_start_date "2024-09-16"
+#./canswim.sh forecast --forecast_start_date "2024-09-16"
+./canswim.sh forecast --forecast_start_date "2024-09-23"
