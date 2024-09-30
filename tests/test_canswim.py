251ee8ad81eb52d0ae8e87967bee106d53fa6d10
ivelin <ivelin117@gmail.com>
2024-09-30 09:18:00 -0500

    test: cursor autocomplete
    Signed-off-by: ivelin <ivelin117@gmail.com>

--- tests/test_canswim.py
+++ tests/test_canswim.py
@@ -5,6 +5,7 @@ from canswim.model import CanswimModel
 def test_canswim_model_initialization():
     model = CanswimModel()
     assert isinstance(model, CanswimModel)
+    assert model.model is None
 
 
 def test_canswim_model_build():
