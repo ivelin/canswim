f10a29752d4d0dacfc9afd1232df342b28ea0f01
ivelin <ivelin117@gmail.com>
2025-02-22 20:02:33 -0600

    fix: revert data upload to tgz. Hitting API limit with parquet files.
    Signed-off-by: ivelin <ivelin117@gmail.com>

--- weekend.sh
+++ weekend.sh
@@ -1,11 +1,29 @@
 #!/usr/bin/bash
+#args=("$@")
+
+# This script is intended to be executed by cron each weekend.
+# The goal is to pull latest market data for the week 
+# and run forecast for the following week.
 
 # stop on first error
-set -e
+# print verbose messages
+set -ex
+
+echo "CANSWIM Weekend Forecast Routine: Starting..."
+
+conda activate canswim
+pip install -e ./
+
+#python -m canswim "${args[@]}"
+
+python -m canswim 
 
 # gather up to date market data
-./canswim gatherdata
+##./canswim.sh gatherdata
 
 # run forecast and upload to hf hub
-./canswim forecast
+##./canswim.sh forecast
+
+echo "CANSWIM Weekend Forecast Routine: Finished."
+
 
